fc11d02c7a1d1a4f702d0f5003d19d0717816662
Fielding Johnston <fielding@justfielding.com>
2018-02-15 07:29:03 -0600

    Add gradient border at the top of the page

--- components/Page.js
+++ components/Page.js
@@ -6,6 +6,11 @@ const Page = ({ children }) => (
     <style jsx global>
       {`
         body {
+          border-top: 4px solid #26A9E0;
+          border-image-source: linear-gradient(to left, #2f80ed, #56ccf2);
+          border-image-slice: 4;
+          margin: 0;
+          padding: 0;
           font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
         }
 

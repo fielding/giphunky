8f5d91245fa64c61c70cd4f66eb45bd74c5f9f90
Fielding Johnston <fielding@justfielding.com>
2018-02-15 01:34:10 -0600

    Debounce the onScroll for infinite scrolling with 250ms wait

--- components/GiphySearch/withInfiniteScroll.js
+++ components/GiphySearch/withInfiniteScroll.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import debounce from '../../lib/debounce.js';
 
 const withInfiniteScroll = conditionFn => Component =>
   class WithInfiniteScroll extends React.Component {
+
     componentDidMount() {
-      window.addEventListener('scroll', this.onScroll, false);
+      window.addEventListener('scroll', this.debounced, false);
     }
 
     componentWillUnmount() {
-      window.removeEventListener('scroll', this.onScroll, false);
+      window.removeEventListener('scroll', this.debounced, false);
     }
 
     onScroll = () => {
       conditionFn(this.props) && this.props.onPaginatedSearch();
     }
+
+    debounced = debounce(this.onScroll, 250);
 
     render() {
       return <Component {...this.props} />;

7f0769a2eb44ad6d7348464783b4e25bdea1b11a
Fielding Johnston <fielding@justfielding.com>
2018-02-15 05:33:05 -0600

    Add page title

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
 
 import Page from '../components/Page';
 import GiphySearch from '../components/GiphySearch';
 
 const Index = () => (
   <Page>
+    <Head>
+      <title>infinite scroll giphy search</title>
+    </Head>
     <GiphySearch />
   </Page>
 );

f5416a34ab59350cb9d458922798683217e33a39
Fielding Johnston <fielding@justfielding.com>
2018-02-20 23:35:59 -0600

    Add webpack loaders and configuration needed to allow for external css

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,13 @@ import Head from 'next/head';
 import Page from '../components/Page';
 import GiphySearch from '../components/GiphySearch';
 
+import stylesheet from '../styles/index.css';
+
 const Index = () => (
   <Page>
     <Head>
       <title>infinite scroll giphy search</title>
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     </Head>
     <GiphySearch />
   </Page>

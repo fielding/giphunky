62599a3feb8591d37738d1bfdb8ed5e71bdc46e2
Fielding Johnston <fielding@justfielding.com>
2019-05-25 16:29:43 -0500

    Temporary workaround for netlify/netlify-lambda#142; Prettify lambda fn
    PR - netlify/zip-it-and-ship-it#34

--- lambda/giphyApiSearch.js
+++ lambda/giphyApiSearch.js
@@ -1,14 +1,22 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch').default;
 
 const getGiphySearchUrl = (value, page) =>
-  `https://api.giphy.com/v1/gifs/search?q=${value}&api_key=${process.env.GIPHY_API_KEY}&limit=20&offset=${page * 20}`;
+  `https://api.giphy.com/v1/gifs/search?q=${value}&api_key=${
+    process.env.GIPHY_API_KEY
+  }&limit=20&offset=${page * 20}`;
 
 exports.handler = (event, context, callback) => {
   const { value, page } = event.queryStringParameters;
 
   return fetch(getGiphySearchUrl(value, page))
     .then(response => response.json())
-    .then(json => ({ statusCode: 200, body: JSON.stringify({ data: json.data, pagination: json.pagination, meta: json.meta }) }))
+    .then(json => ({
+      statusCode: 200,
+      body: JSON.stringify({
+        data: json.data,
+        pagination: json.pagination,
+        meta: json.meta,
+      }),
+    }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
-

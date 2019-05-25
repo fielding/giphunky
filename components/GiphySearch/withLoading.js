78279788b280478c4226f6d3deb648d0e6127370
Fielding Johnston <fielding@justfielding.com>
2019-05-25 04:20:01 -0500

    Prettify withLoading.js

--- components/GiphySearch/withLoading.js
+++ components/GiphySearch/withLoading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const withLoading = conditionFn => Component => props =>
-  (<div>
+const withLoading = conditionFn => Component => props =>(
+  <div>
     <Component {...props} />
 
     <div className="loading">
@@ -14,6 +14,7 @@ const withLoading = conditionFn => Component => props =>
         }
       `}
     </style>
-  </div>);
+  </div>
+  );
 
 export default withLoading;

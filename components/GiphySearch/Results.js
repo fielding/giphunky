39c6a36104ffb33d537faa7ec416e7a8b8fff5c5
Fielding Johnston <fielding@justfielding.com>
2019-05-26 09:52:16 -0500

    Update infiniteScrollCondition: how it determines when to fetch data

--- components/GiphySearch/Results.js
+++ components/GiphySearch/Results.js
@@ -7,19 +7,22 @@ import withInfiniteScroll from './withInfiniteScroll';
 const paginatedCondition = props =>
   props.page !== null && !props.isLoading && props.isError;
 
-const infiniteScrollCondition = props =>
-    (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 250)
-  && props.list.length
-  && !props.isLoading
-  && !props.isError;
+const infiniteScrollCondition = props => {
+  const el = document.scrollingElement || document.documentElement;
+  return (
+    window.innerHeight + el.scrollTop >= el.scrollHeight - 250 &&
+    props.list.length &&
+    !props.isLoading &&
+    !props.isError
+  );
+};
 
-const loadingCondition = props =>
-  props.isLoading;
+const loadingCondition = props => props.isLoading;
 
 const Results = compose(
   withPaginated(paginatedCondition),
   withInfiniteScroll(infiniteScrollCondition),
-  withLoading(loadingCondition),
+  withLoading(loadingCondition)
 )(List);
 
 export default Results;
